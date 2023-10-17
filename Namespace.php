035fd9f66e36d426adaaa88479a0bfa8951e85dc
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-10-17 16:17:45 +0700

    Belajar PHP OOP 2

--- Namespace.php
+++ Namespace.php
@@ -4,14 +4,15 @@ namespace {
 require_once "data/Conflict.php";
 require_once "data/Helper.php";
 
-use Data\One\Conflict;
-use function Helper\helpMe;
-use const Helper\APPLICATION;
+use Data\One\Conflict as Conflict1;
+use Data\Two\Conflict as Conflict2;
+use function Helper\helpMe as help;
+use const Helper\APPLICATION as APP;
 
-$conflict1 = new Conflict();
-$conflict2 = new Data\Two\Conflict();
+$conflict1 = new Conflict1();
+$conflict2 = new Conflict2();
 
-echo APPLICATION . PHP_EOL;
+echo APP . PHP_EOL;
 
-helpMe();
+help();
 }
