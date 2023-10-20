f67356aae89f7826f2287d0d980e1c2eb2964195
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-10-21 06:26:04 +0700

    Belajar PHP OOP 6

--- Reflection.php
+++ Reflection.php
@@ -5,5 +5,22 @@ require_once "data/LoginRequest.php";
 require_once "helper/ValidationUtil.php";
 
 $request = new LoginRequest();
+$request->username = null;
+$request->password = null;
 
-ValidationUtill::validate($request);
+ValidationUtill::validateReflection($request);
+
+// ValidationUtill::validate($request);
+
+class RegisterRequest {
+    public ?string $name;
+    public ?string $address;
+    public ?string $email;
+}
+
+$register = new RegisterRequest();
+$register->name = "eko";
+$register->address = "Subang";
+$register->email = "eko@gmail.com";
+
+ValidationUtill::validateReflection($register);
